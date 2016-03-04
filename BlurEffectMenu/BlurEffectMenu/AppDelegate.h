33ffb52db61d56c67bde4264c985fc9978c9b1ff
joinup <shen_sgh@icloud.com>
2016-03-04 22:10:49 +0800

    create BlurEffectMenu  code

---
+++ BlurEffectMenu/BlurEffectMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlurEffectMenu
+//
+//  Created by shen_gh on 16/3/4.
+//  Copyright © 2016年 com.joinup(Beijing). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
